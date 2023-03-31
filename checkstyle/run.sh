20da730ddf826aec3aebf75fcf946a1e16688fb0
Freeeeeeeeedom <709034836@qq.com>
2023-03-31 20:36:08 +0800

    docs: checkstyle report import

---
+++ checkstyle/run.sh
@@ -0,0 +1,32 @@
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/BoosterWordsList.java -o ./reports/BoosterWordsList.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/ClassificationOptions.java -o ./reports/ClassificationOptions.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/ClassificationResources.java -o ./reports/ClassificationResources.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/ClassificationStatistics.java -o ./reports/ClassificationStatistics.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/Corpus.java -o ./reports/Corpus.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/CorrectSpellingsList.java -o ./reports/CorrectSpellingsList.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/EmotionsList.java -o ./reports/EmotionsList.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/EvaluativeTerms.java -o ./reports/EvaluativeTerms.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/IdiomList.java -o ./reports/IdiomList.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/IronyList.java -o ./reports/IronyList.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/Lemmatiser.java -o ./reports/Lemmatiser.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/NegatingWordList.java -o ./reports/NegatingWordList.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/Paragraph.java -o ./reports/Paragraph.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/QuestionWords.java -o ./reports/QuestionWords.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/Sentence.java -o ./reports/Sentence.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/SentimentWords.java -o ./reports/SentimentWords.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/SentiStrength.java -o ./reports/SentiStrength.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/Term.java -o ./reports/Term.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/Test.java -o ./reports/Test.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/TextParsingOptions.java -o ./reports/TextParsingOptions.xml
+java -jar ./checkstyle-10.9.3-all.jar -c ./Google_style.xml -f xml ../src/main/java/nju/SEIII/EASIEST/sentistrength/UnusedTermsClassificationIndex.java -o ./reports/UnusedTermsClassificationIndex.xml
+
+
+
+
+
+
+
+
+
+
+
